39c98480fb6782a8aa5b001504f0c737bbefd67f
ahmadasnawi <ahmadaries899@gmail.com>
2024-09-08 14:32:13 +0700

    task 15

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Font Awesome Cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="./src/css/main.css" />
+    <!-- JS -->
+    <script src="./src/js/app.js" defer></script>
+    <title>Disorganized</title>
+</head>
+
+<body>
+    <section>
+        <nav>
+            <div class="left-line">
+                <ul>
+                    <li>*</li>
+                </ul>
+                <ul>
+                    <li>Services</li>
+                    <li>About application</li>
+                    <li>Our mission</li>
+                </ul>
+            </div>
+            <div class="right-line">
+                <ul>
+                    <li><i class="fa-brands fa-opera"></i></li>
+                    <li><i class="fa-brands fa-instagram"></i></li>
+                    <li><i class="fa-brands fa-twitter"></i></li>
+                    <li>Download now</li>
+                </ul>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li>Menu</li>
+                </ul>
+            </div>
+        </nav>
+        <header>
+            <div class="left-side">
+                <h6>YOUR APPLICATION</h6>
+                <h1>Securing Your Online <br> Payments Methods.</h1>
+                <p>Recomended secure <b><u>payment systems</u></b> for mobile application to manage own <br>resources and transfer between users.</p>
+                <button>
+                    <p>Get started</p>
+                    <i class="fa-solid fa-arrow-right"></i>
+                </button>
+                <div class="under-line">
+                    <div class="description">
+                        <div class="top">
+                            <i class="fa-solid fa-check"></i>
+                            <div></div>
+                        </div>
+                        <h1>Safe Payments</h1>
+                        <p><b><u>Universal solution</b></u> for private customers <br>and business companies.</p>
+                    </div>
+                    <div class="description-1">
+                        <div class="top">
+                            <i class="fa-solid fa-check"></i>
+                            <div></div>
+                        </div>
+                        <h1>Friendly User Interface</h1>
+                        <p>Created with a passion for design and <br>easy discovering new features in the <br>mobile software.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="mid-side">
+                <div class="top-box">
+                    <i class="fa-solid fa-check"></i>
+                    <h1>Verify your payment</h1>
+                    <p>Enter your digital code</p>
+                    <div class="code">
+                        <div>3</div>
+                        <div>2</div>
+                        <div>4</div>
+                        <div>1</div>
+                    </div>
+                    <div class="garis">
+                        <div class="garis-a"></div>
+                        <div class="garis-b"></div>
+                    </div>
+                    <span>New account? <u>Send a new code.</u></span>
+                </div>
+                <div class="mid-box">
+                    <h1>Payment sent positively.</h1>
+                    <p>check the operation details.</p>
+                </div>
+                <div class="bottom-box">
+                    <div class="box">
+                        <div class="left-1">
+                            <i class="fa-regular fa-newspaper"></i>
+                            <div class="deskripsi">
+                                <h>MoonShop Store</h>
+                                <p>21 August 2023</p>
+                            </div>
+                        </div>
+                        <div class="titik"></div>
+                    </div>
+                    <div class="garis"></div>
+                </div>
+            </div>
+            <div class="right-side">
+                <div class="atas">
+                    <div class="top">
+                        <p>Secure Solutions</p>
+                        <div class="titik">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div class="bottom">
+                        <h1>Online</h1> 
+                        <div class="under">
+                            <h1>Payments</h1>
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="tengah">
+                    <div class="title">
+                        <p>****2934</p>
+                        <div class="titek">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div class="deskrip">
+                        <h1>4.383,25 <span>usd</span></h1>
+                        <div class="under-line">
+                            <p>Total Balance</p>
+                            <i class="fa-solid fa-arrow-down-long"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="bawah">
+                    <div class="line-1">
+                        <p>Latest Massages</p>
+                        <i class="fa-solid fa-arrow-down-wide-short"></i>
+                    </div>
+                    <div class="line-2">
+                        <div class="left">
+                            <h1>T</h1>
+                        </div>
+                        <div class="right">
+                            <div class="atas-1">
+                                <div class="description-1">
+                                    <p><b>Thomash smith</b></p>
+                                    <div></div>
+                                </div>
+                                <span>6 min</span>
+                            </div>
+                            <div class="bawah-1">
+                                <p>Thanks for checking!</p>
+                                <span>2</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="line-3">
+                        <div class="left">
+                            <h1>j</h1>
+                        </div>
+                        <div class="right">
+                            <div class="atas-1">
+                                <div class="description-2">
+                                    <p><b>Jack Simon</b></p>
+                                    <div></div>
+                                </div>
+                                <span>15 min</span>
+                            </div>
+                            <div class="bawah-1">
+                                <p>New payment! Thank you!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </section>
+</body>
+
+</html>
